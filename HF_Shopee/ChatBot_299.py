37a53859b3f777ccc500a0372c85d6b7c0bce336
Shaik.Fardeen <70197445+190031489Fardeen@users.noreply.github.com>
2021-12-13 11:15:18 +0530

    Add files via upload

---
+++ HF_Shopee/ChatBot_299.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def sdp_299():
+    sum = 0
+    print("Hello!")
+    print("Welcome to HF Shopee.. We Guide You !!")
+    print("We would like you to register in our portal")
+    print("Can I know your name?")
+    name = raw_input ("Enter your name: ")
+    print("Hello!!",name)
+    print("Can I know where are you from",name)
+    address = raw_input("Enter your address details: ")
+    print("Enter your mobile number")
+    mobilenum = raw_input()
+    print("Create a password which contains alphanumeric characters of min length 8")
+    password = raw_input()
+    print("Thank you for registering in HF Shopee")
+    print("I would like to display all the facilities provided by our agency")
+    print("What kind of Urban or Rural Area")
+    print("Type Urban or Rural")
+    area = raw_input()
+    if area =="urban":
+        print("Here are the places which our Delivery:\n1. Hyderabad\n2. Mumbai\n3. Kolkata")
+        print("Select a number from the displayed places")
+        place = raw_input()
+        if place == "1":
+            print("Which mode of Deliver you would prefer to your the destination?")
+            print("1. Fast\n2. Normal\n3. On-Demand Delivery\n4. Scheduled On-Demand Deliver" )
+            print("Select a number for the mode of Delivery you prefer to go")
+            mode = raw_input()
+            if mode == "1":
+                sum = sum + 51
+                print("Amount required for Fast Delivery is ", sum)
+            if mode == "2":
+                sum = sum + 30
+                print("Amount required for Normal Delivery is ", sum)
+            if mode == "3":
+                sum = sum + 65
+                print("Amount required for On-Demand Delivery is ", sum)
+            if mode == "4":
+                sum = sum + 76
+                print("Amount required for Scheduled On-Demand Delivery is ", sum)
+        if place == "2":
+            print("Which mode of Deliver you would prefer to your the destination?")
+            print("1. Fast\n2. Normal\n3. On-Demand Delivery\n4. Scheduled On-Demand Deliver" )
+            print("Select a number for the mode of Delivery you prefer to go")
+            mode = raw_input()
+            if mode == "1":
+                sum = sum + 51
+                print("Amount required for Fast Delivery is ", sum)
+            if mode == "2":
+                sum = sum + 30
+                print("Amount required for Normal Delivery is ", sum)
+            if mode == "3":
+                sum = sum + 65
+                print("Amount required for On-Demand Delivery is ", sum)
+            if mode == "4":
+                sum = sum + 76
+                print("Amount required for Scheduled On-Demand Delivery is ", sum)
+        if place == "3":
+            print("Which mode of Deliver you would prefer to your the destination?")
+            print("1. Fast\n2. Normal\n3. On-Demand Delivery\n4. Scheduled On-Demand Deliver" )
+            print("Select a number for the mode of Delivery you prefer to go")
+            mode = raw_input()
+            if mode == "1":
+                sum = sum + 51
+                print("Amount required for Fast Delivery is ", sum)
+            if mode == "2":
+                sum = sum + 30
+                print("Amount required for Normal Delivery is ", sum)
+            if mode == "3":
+                sum = sum + 65
+                print("Amount required for On-Demand Delivery is ", sum)
+            if mode == "4":
+                sum = sum + 76
+                print("Amount required for Scheduled On-Demand Delivery is ", sum)
+    if area =="rural":
+        print("Here are the places which our Delivery:\n1. Chiral\n2. Bapatla\n3. Ponnur")
+        print("Select a number from the displayed places")
+        place = raw_input()
+        if place == "1":
+            print("Which mode of Deliver you would prefer to your the destination?")
+            print("1. Fast\n2. Normal\n3. On-Demand Delivery\n4. Scheduled On-Demand Deliver" )
+            print("Select a number for the mode of Delivery you prefer to go")
+            mode = raw_input()
+            if mode == "1":
+                sum = sum + 51
+                print("Amount required for Fast Delivery is ", sum)
+            if mode == "2":
+                sum = sum + 30
+                print("Amount required for Normal Delivery is ", sum)
+            if mode == "3":
+                sum = sum + 65
+                print("Amount required for On-Demand Delivery is ", sum)
+            if mode == "4":
+                sum = sum + 76
+                print("Amount required for Scheduled On-Demand Delivery is ", sum)
+        if place == "2":
+            print("Which mode of Deliver you would prefer to your the destination?")
+            print("1. Fast\n2. Normal\n3. On-Demand Delivery\n4. Scheduled On-Demand Deliver" )
+            print("Select a number for the mode of Delivery you prefer to go")
+            mode = raw_input()
+            if mode == "1":
+                sum = sum + 51
+                print("Amount required for Fast Delivery is ", sum)
+            if mode == "2":
+                sum = sum + 30
+                print("Amount required for Normal Delivery is ", sum)
+            if mode == "3":
+                sum = sum + 65
+                print("Amount required for On-Demand Delivery is ", sum)
+            if mode == "4":
+                sum = sum + 76
+                print("Amount required for Scheduled On-Demand Delivery is ", sum)
+        if place == "3":
+            print("Which mode of Deliver you would prefer to your the destination?")
+            print("1. Fast\n2. Normal\n3. On-Demand Delivery\n4. Scheduled On-Demand Deliver" )
+            print("Select a number for the mode of Delivery you prefer to go")
+            mode = raw_input()
+            if mode == "1":
+                sum = sum + 51
+                print("Amount required for Fast Delivery is ", sum)
+            if mode == "2":
+                sum = sum + 30
+                print("Amount required for Normal Delivery is ", sum)
+            if mode == "3":
+                sum = sum + 65
+                print("Amount required for On-Demand Delivery is ", sum)
+            if mode == "4":
+                sum = sum + 76
+                print("Amount required for Scheduled On-Demand Delivery is ", sum)
+            
+sdp_299();
+
+
+# In[ ]:
+
+
+
+
---
+++ HF_Shopee/HTML Template/jfPopUp/jquery.jfPopUp.js
@@ -0,0 +1,119 @@
+(function($) {
+
+
+    $.jfPopUp = function(element, options) {
+        var plugin = this;
+        var $element = $(element);
+        var $ldElement;
+        var $mb;
+        var $mbWin;
+        var dataatts = $element.data();
+        var win = false;        
+
+        var defaults = {
+            loadElement: 'body',
+            message:"Hello :)",
+            auto:false,
+            mouseEvent:'none',
+            animationFrom:{opacity:'0', 'margin-top':'100px'},
+            animationTo:{opacity:'1', 'margin-top':'0px'},
+            pause:0,
+            speed:250,
+            ease:'swing',
+            onStart: function() {}, 
+            onStartArgs: [],
+            onComplete: function() {}, 
+            onCompleteArgs: [],
+            onClosed: function() {}, 
+            onClosedArgs: []
+        };
+        plugin.settings = {};
+
+        plugin.init = function() {
+            plugin.settings = $.extend({}, defaults, options, dataatts);             
+            $ldElement = $(plugin.settings.loadElement);
+
+            if (plugin.settings.mouseEvent !=='none'){
+                $element.bind(plugin.settings.mouseEvent, onMouse);
+            }
+        };
+        // mouse event
+        function onMouse(event){
+            event.preventDefault();              
+            plugin.launch();
+
+        }
+        
+       
+        function completed(){
+            // add close functionality
+            $('.mb_closeBtn, .mb_shade').bind('click',plugin.close); 
+            // call on complete function
+            plugin.settings.onComplete.apply(plugin,plugin.settings.onCompleteArgs); 
+        }
+        plugin.close = function(){
+            $mbWin.animate(plugin.settings.animationFrom, plugin.settings.speed, plugin.settings.ease, 
+                function(){    
+                $mb.animate({opacity:'.85'},plugin.settings.speed, "", 
+                    function(){     
+                        $('.mb_closeBtn, .mb_shade').unbind('click');
+                        $mb.remove();
+                        plugin.settings.onClosed.apply(plugin,plugin.settings.onClosedArgs);
+                        win = false;
+                });
+            });  
+        };
+        plugin.destroy = function(){
+            if (win){
+                plugin.close();
+            }
+            $element.removeData('jfPopUp', plugin);
+            plugin = null;
+        };
+       
+        plugin.launch = function(){
+            //------------------------ launch function
+            win = true;
+            // call on start function
+            plugin.settings.onStart.apply(plugin,plugin.settings.onStartArgs);
+            // make tags
+            var tag = [];
+            tag.push('<div class="mb_messagebox">');
+            tag.push('<div class="mb_shade"></div>');
+            tag.push('<div class="mb_window">');    
+            tag.push('<div class="mb_content">'+plugin.settings.message+'</div>');     
+            tag.push('<div class="mb_closeBtn"></div>');
+            tag.push('</div></div>');
+            var tagString = '';
+            $(tag).each(function(index, element){
+                tagString+=element;
+            });
+            $ldElement.prepend(tagString);
+            // cache objects 
+            $mb = $('.mb_messagebox');  
+            $mbWin = $('.mb_window');
+          
+            // shade animation
+            $('.mb_shade').css({opacity:'0'}).animate({opacity:'1'}, plugin.settings.speed);
+            // set, load, and animate window
+            $mbWin.css(plugin.settings.animationFrom); 
+            $mbWin.delay(plugin.settings.pause).animate(plugin.settings.animationTo, plugin.settings.speed, plugin.settings.ease, completed);    
+        };  
+        plugin.init();
+
+
+        if (plugin.settings.auto){
+            plugin.launch();
+        }
+
+    };
+
+    $.fn.jfPopUp = function(options) {
+        return this.each(function() {
+            if (undefined === $(this).data('jfPopUp')) {
+                var plugin = new $.jfPopUp(this, options);
+                $(this).data('jfPopUp', plugin);
+            }
+        });
+    };
+})(jQuery);
